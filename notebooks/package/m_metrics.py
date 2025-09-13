5712e2c60b0d35e6936f9f1820f4dc7ca05b7401
JoranVerspreet <jverspreet@protonmail.com>
2025-09-13 08:58:40 +0200

    implementing functions for assymatric weight MAE, train_test_split, and defining baseline model

--- notebooks/package/m_metrics.py
+++ notebooks/package/m_metrics.py
@@ -17,5 +17,5 @@ def f_compute_metrics(y_true, y_pred, alpha=0.333, wi=1.25):
     rmse = np.sqrt(mean_squared_error(y_true, y_pred))
     mae = mean_absolute_error(y_true, y_pred)
     mape = np.mean(np.abs((y_true - y_pred) / np.maximum(y_true, 1e-6))) * 100
-    asym_wmae_score = asym_wmae(y_true, y_pred, alpha=alpha, wi=wi)
+    asym_wmae_score = f_asym_wmae(y_true, y_pred, alpha=alpha, wi=wi)
     return {'RMSE': rmse, 'MAE': mae, 'MAPE': mape, 'Asym_WMAE': asym_wmae_score}

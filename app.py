bb8430d7605e83d106f70721acc7c3c276dd927f
JoranVerspreet <jverspreet@protonmail.com>
2026-08-03 20:48:34 +0200

    jj

--- app.py
+++ app.py
@@ -174,10 +174,10 @@ sns.lineplot(
     ax=ax,
     marker="o"
     )
-ax.set_ylim(4.5, 5.5)
+ax.set_ylim(0, 35)
 plt.setp(ax.get_xticklabels(), rotation=45, ha="right")
 ax.set(xlabel="")
 
-ax.set_ylabel("Pace (min/km)")
+ax.set_ylabel("Pace (km/h)")
 ax.set_xlabel("Month")
 st.pyplot(fig)

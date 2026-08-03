575f61f3a7f0f8a49a3c11e77f12877851d49c9e
JoranVerspreet <jverspreet@protonmail.com>
2026-08-03 20:40:58 +0200

    improve plot

--- app.py
+++ app.py
@@ -138,6 +138,9 @@ sns.lineplot(
     marker="o"
     )
 ax.set_ylim(4.5, 5.5)
+plt.setp(ax.get_xticklabels(), rotation=45, ha="right")
+ax.set(xlabel=None)
+
 
 ax.set_ylabel("Pace (min/km)")
 ax.set_xlabel("Month")
